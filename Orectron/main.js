108ab8b9730a54be70af263a5f271e2a5deb7f22
Uchio KONDO <udzura@pepabo.com>
2016-02-29 19:39:30 +0900

    Context menu sample

--- Orectron/main.js
+++ Orectron/main.js
@@ -3,6 +3,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = require('menu');
 
 app.on('window-all-closed', function() {
   app.quit();
@@ -15,6 +16,20 @@ app.on('ready', function() {
         center: true,
         title: "Remote Demo だぎゃあ",
     });
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: "おっす",
+            submenu: [{
+                label: 'やるぞ！',
+                accelerator: 'Shift+CmdOrCtrl+D',
+                click: function() {
+                    BrowserWindow.getFocusedWindow().toggleDevTools();
+                }
+            }]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     //mainWindow.webContents.openDevTools();
